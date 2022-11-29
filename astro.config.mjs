aadce0a3c37083a4f1a568c9eced0430b5808b7f
Trobo <57040351+trobonox@users.noreply.github.com>
2022-11-29 07:49:34 +0100

    feat: add vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), vue()]
 });
