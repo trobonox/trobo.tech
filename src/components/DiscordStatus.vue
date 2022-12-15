45fa80ec4cdf20da70b01293e083000598915f81
Trobo <57040351+trobonox@users.noreply.github.com>
2022-12-15 18:53:26 +0100

    feat: adjust lanyard logic to prevent some flickering

--- src/components/DiscordStatus.vue
+++ src/components/DiscordStatus.vue
@@ -6,7 +6,7 @@
                 alt="Trobonox Profile Picture"
                 class="rounded-full w-48 h-48 object-cover"
             />
-            <span v-if="!offline" data-text="Discord Status: online" class="tooltip w-8 h-8 rounded-full bg-emerald-500 border-4 border-emerald-600 absolute right-4 bottom-0.5" />
+            <span v-if="!offline && !loading" data-text="Discord Status: online" class="tooltip w-8 h-8 rounded-full bg-emerald-500 border-4 border-emerald-600 absolute right-4 bottom-0.5" />
             <span v-else data-text="Currently offline" class="tooltip w-8 h-8 rounded-full bg-gray-500 border-4 border-gray-600 absolute right-4 bottom-0.5" />
         </div>
     </div>
@@ -18,8 +18,11 @@
     const lanyard_url = "https://api.lanyard.rest/v1/users/540898474288480256";
     const lanyard: Ref<any> = ref(null);
 
+    const loading = ref(true);
+
     onMounted(async () => {
         lanyard.value = await fetch(lanyard_url).then((response) => response.json());
+        loading.value = false;
     });
 
     const offline = computed(() => {
